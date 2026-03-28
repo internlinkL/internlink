a5d7b960ba503e949c4f66df5d637093eda72310
chahdmeriane <chahd.meriane2004@gmail.com>
2026-03-28 02:41:04 +0100

    Update login.php

--- phpsecure/login.php
+++ phpsecure/login.php
@@ -2,6 +2,8 @@
 error_reporting(0);
 ini_set('display_errors', 0);
 header('Content-Type: application/json');
+
+session_name('internlink_session');
 session_start();
 require_once __DIR__ . '/db.php';
 

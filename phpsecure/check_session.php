d90b6f3642f22f726c615fbca2f06b86f64073b8
ikramblm <ikramblmdh7@gmail.com>
2026-04-10 19:25:41 +0100

    checksession

---
+++ phpsecure/check_session.php
@@ -0,0 +1,11 @@
+<?php
+session_name('internlink_session');
+session_start();
+header('Content-Type: application/json');
+
+if (isset($_SESSION['user_id'])) {
+    echo json_encode(['loggedIn' => true, 'role' => $_SESSION['role']]);
+} else {
+    echo json_encode(['loggedIn' => false]);
+}
+?>
